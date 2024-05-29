77d5bb22535ec0ac6c36c7d94d8e41da7ec3f526
Mac Gould <mgouldspam@gmail.com>
2024-05-29 15:58:45 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Plutonode Hosting Knowledge Base</span>,
-  docsRepositoryBase: 'https://github.com/spoopy2023/plutonode-documentation/tree/main/docs',
+  docsRepositoryBase: 'https://github.com/Spoopy2023/plutonode-documentation/tree/main',
   footer: {
     text: 'Plutonode Hosting 2023-2024',
   },
