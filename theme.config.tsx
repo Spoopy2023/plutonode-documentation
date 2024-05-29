f6c78bbaeb3ab6655c416f1bc2b834bcd54e08a4
Mac Gould <mgouldspam@gmail.com>
2024-05-29 15:57:40 +0100

    Base files

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>Plutonode Hosting Knowledge Base</span>,
+  docsRepositoryBase: 'https://github.com/spoopy2023/plutonode-documentation/tree/main/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Plutonode Hosting 2023-2024',
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+    </>
+  )
 }
 
 export default config
